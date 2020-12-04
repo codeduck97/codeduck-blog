a12375780e0ecaf5b50d12e408849a26660f83d6
jason <1281028036@qq.com>
2020-12-04 19:48:11 +0800

    修复管理系统

--- codeduck-blog-admin-vue/public/static/ckeditor/plugins/video/lang/en.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.plugins.setLang('video', 'en', {
-  button: 'Video',
-  title: 'Video properties',
-  emptySrc: 'URL must not be empty.',
-  controls: 'Enable controls',
-  mutedLoopingAutoplay: 'Muted looping autoplay',
-  preview: 'Preview',
-  invalidSrc: 'URL is invalid or video is not playable.'
-});
--- codeduck-blog-admin-vue/public/static/ckeditor/plugins/video/lang/fr.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.plugins.setLang('video', 'fr', {
-  button: 'Vidéo',
-  title: 'Propriétés de la vidéo',
-  emptySrc: 'L’URL doit être indiquée.',
-  controls: 'Activer les contrôles',
-  mutedLoopingAutoplay: 'Lecture automatique en boucle et muette',
-  preview: 'Aperçu',
-  invalidSrc: 'L’URL est invalide ou la vidéo n’est pas lisible.'
-});
