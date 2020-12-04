9c56d7793fd618804c16d97c468ad89bc842419f
jason <1281028036@qq.com>
2020-12-04 20:22:21 +0800

    删除旧admin-vue

--- codeduck-blog-admin-vue/src/utils/checkPermission.js
+++
@@ -1,6 +0,0 @@
-import store from '@/store'
-
-export function hasPermission(permission) {
-  const myPermissions = store.getters.permissions
-  return myPermissions.indexOf(permission) > -1
-}
--- codeduck-blog-admin-vue/src/views/profile/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>待实现……</h1>
-  </div>
-</template>
